75e0c4a9eed8deec17709a94c66ecc83044b8b7a
Vishnupriya K <vishnupriya@autosmarts.co>
2025-04-13 23:45:59 +0530

    route

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,10 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   routes: [
- 
+    {
+      path: '/',
+      redirect: '/home'
+    },
     {
       path: '/home',
       name: 'home-section',
